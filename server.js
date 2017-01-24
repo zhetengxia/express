46e84b4c60c15cf57769f5197fc3858a2daeb700
“zhetengxia” <zhetengxia@qq.com>
2017-01-24 21:57:59 +0800

    1

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Last, but not least, listen on port 8080
+server.listen(8080);
